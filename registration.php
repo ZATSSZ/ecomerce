4508f26e87a2c9146db88966988baab9e825811d
ZATSSZ <ZATSSZ>
2024-11-11 23:17:37 +0800

    update product to database

--- registration.php
+++ registration.php
@@ -69,9 +69,6 @@
             </div>
         </div>
     </div>
-    <!-- Bootstrap 5 JS Bundle -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+   
 
     <?php require_once("includes/footer.php") ?>
-</body>
-</html>
